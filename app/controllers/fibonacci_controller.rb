811a613bf05322df1dcf7183d7086f4ba0764b2f
Tasaki-Kazuki <kazuki01gk@icloud.com>
2023-03-19 23:40:19 +0900

    feat: フィボナッチ数を求める関数とコントローラーを追加

--- app/controllers/fibonacci_controller.rb
+++ app/controllers/fibonacci_controller.rb
@@ -1,2 +1,27 @@
 class FibonacciController < ApplicationController
+  def fib
+    n = params[:n].to_i
+
+    response = { n: n, fib: calculate_fibonacci(n) }
+    render json: response
+  end
+
+  private
+
+  def calculate_fibonacci(n)
+    return 0 if n == 0
+    return 1 if n == 1
+
+    before_last_num = 0
+    last_num = 1
+    fib_num = 0
+
+    (2..n).each do |i|
+      fib_num = last_num + before_last_num
+      before_last_num = last_num
+      last_num = fib_num
+    end
+
+    return fib_num
+  end
 end
